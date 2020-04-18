94edba7a819994f414a94e3c4bcea82c98cf29bb
Wei Huan Chen <32426660+whc105@users.noreply.github.com>
2020-04-18 14:22:57 -0600

    Add files via upload

---
+++ find_frequent.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+
+yelp_review = pd.read_csv("yelp_review.csv")
+
+highest_reviews = yelp_review.groupby(['business_id'])['business_id'].count().reset_index(name='count').sort_values(['count'], ascending=False).head(10)
+
+print(highest_reviews)
+
+result = yelp_review.loc[yelp_review['business_id'].isin(highest_reviews['business_id'])]
+
+result.drop(columns=['review_id', 'user_id'], inplace=True)
+
+result.to_csv (r'parsed_workable.csv', index = False, header=True)
